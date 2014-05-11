2fe7fdaefbde4237c3bf71278f1b95c7fd7478d4
ankushkumar <kr_ankush@hotmail.com>
2014-05-11 16:12:22 +0530

    hello word example
    hello_word example in objective -c

---
+++ hello_world/firstclass.h
@@ -0,0 +1,13 @@
+//
+//  firstclass.h
+//  hello_world
+//
+//  Created by Ankush Kumar on 06/05/14.
+//  Copyright (c) 2014 Ankush Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface firstclass : NSObject
+
+@end
---
+++ hello_world/firstclass.m
@@ -0,0 +1,13 @@
+//
+//  firstclass.m
+//  hello_world
+//
+//  Created by Ankush Kumar on 06/05/14.
+//  Copyright (c) 2014 Ankush Kumar. All rights reserved.
+//
+
+#import "firstclass.h"
+
+@implementation firstclass
+
+@end
